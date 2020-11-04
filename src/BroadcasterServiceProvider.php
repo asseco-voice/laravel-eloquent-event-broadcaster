6a6c43c167ab48d16e6afa25eaba7657995d6972
Norgul <Norgul@users.noreply.github.com>
2020-11-04 10:01:27 +0000

    Apply fixes from StyleCI

--- src/BroadcasterServiceProvider.php
+++ src/BroadcasterServiceProvider.php
@@ -4,16 +4,7 @@ declare(strict_types=1);
 
 namespace Voice\EloquentEventBroadcaster;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
-use Voice\Chassis\App\Console\Commands\EnvReplacerCommand;
-use Voice\Chassis\App\Console\Commands\InitChassis;
-use Voice\Chassis\App\Http\Middleware\EndQueryLogMiddleware;
-use Voice\Chassis\App\Http\Middleware\ForceJsonHeaderMiddleware;
-use Voice\Chassis\App\Http\Middleware\LogRequests;
-use Voice\Chassis\App\Http\Middleware\StartQueryLogMiddleware;
 
 class BroadcasterServiceProvider extends ServiceProvider
 {
