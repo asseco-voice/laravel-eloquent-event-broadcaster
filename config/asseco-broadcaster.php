f85b877b52a1c8da7e758b8e5a9150e682086f3d
Marko Dupor <marko.dupor@gmail.com>
2020-12-02 14:15:46 +0100

    Commented out troublesome event

--- config/asseco-broadcaster.php
+++ config/asseco-broadcaster.php
@@ -14,7 +14,7 @@ return [
     'dispatches_events' => [
         'created'   => ModelCreated::class,
         'deleted'   => ModelDeleted::class,
-        'restored'  => ModelRestored::class,
+        // 'restored'  => ModelRestored::class,
         'retrieved' => ModelRetrieved::class,
         'updated'   => ModelUpdated::class,
     ],
