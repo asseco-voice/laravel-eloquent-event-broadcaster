d91cfce61a3cf8c16bd560b243a27853bbd2ced4
Marko Dupor <Norgul@users.noreply.github.com>
2021-01-27 11:31:34 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getPackageProviders($app)
     {
-        return [BroadcasterServiceProvider::class,];
+        return [BroadcasterServiceProvider::class];
     }
 
     protected function getEnvironmentSetUp($app)
